86df4adbff3bbc06f6b2074c6c3273fcfffb2581
SungminSo <soos196@hanyang.ac.kr>
2020-09-05 22:08:56 +0900

    init db and bcrypt in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from .config import config_by_name
+from .models import db, bcrypt
 
 
 def create_app(config_name: str) -> Flask:
+    # app initialization
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
 
+    # initialize db and bcrypt
+    bcrypt.init_app(app)
+    db.init_app(app)
+
+    # for health check
     @app.route('/', methods=['GET'])
     def ping():
         return 'pong'
