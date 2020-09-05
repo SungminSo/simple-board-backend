e3f8daba633d219dab3ad49402f25f8d44ae9802
SungminSo <soos196@hanyang.ac.kr>
2020-09-05 22:16:35 +0900

    set up db migration

--- manage.py
+++ manage.py
@@ -1,8 +1,21 @@
-from app import create_app
+from app import create_app, db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 import os
 
-if __name__ == '__main__':
-    app = create_app(os.getenv('FLASK_ENV') or 'dev')
+app = create_app(os.getenv('FLASK_ENV') or 'dev')
 
+migrate = Migrate(app=app, db=db)
+
+manager = Manager(app=app)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def run():
     app.run()
+
+
+if __name__ == '__main__':
+    manager.run()
