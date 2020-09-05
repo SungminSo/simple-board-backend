85a15162f55e558fdc720fb569c37ead8971bf1d
SungminSo <soos196@hanyang.ac.kr>
2020-09-05 18:05:10 +0900

    create app/__init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
 from flask import Flask
+from .config import config_by_name
+
+
+def create_app(config_name: str) -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(config_by_name[config_name])
+
+    @app.route('/', methods=['GET'])
+    def ping():
+        return 'pong'
+
+    return app
