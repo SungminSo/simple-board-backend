deb9585aac270638b6dca6d49dc6c9a28246fdc5
SungminSo <soos196@hanyang.ac.kr>
2020-09-06 21:49:18 +0900

    separate article apis in view/articles

--- app/views/articles.py
+++ app/views/articles.py
@@ -8,7 +8,7 @@ from ..shared.auth import Auth
 article_api = Blueprint('article', __name__)
 
 
-@article_api.route('article/<string:board>/<int:limit>/articles/<int:page>', methods=['GET'])
+@article_api.route('/article/<string:board>/<int:limit>/articles/<int:page>', methods=['GET'])
 @Auth.token_required
 def get_articles(board: str, limit: int, page: int):
     board = Board.find_board_by_uuid(board)
@@ -30,74 +30,77 @@ def get_articles(board: str, limit: int, page: int):
     return json_response({'total': len(articles), 'articles': ret_articles}, 200)
 
 
-@article_api.route("/article", methods=['GET', 'POST', 'PATCH', 'DELETE'])
+@article_api.route('/article', methods=['POST'])
 @Auth.token_required
-def article_views():
-    if request.method == 'POST':
-        try:
-            req_data = request.get_json()
-            title = req_data['title']
-            content = req_data['content']
-            board_uuid = req_data['board_uuid']
-        except TypeError:
-            return json_response({'errorMsg': 'please send request data'}, 400)
-        except KeyError:
-            return json_response({'errorMsg': 'please check your request data'}, 400)
+def create_article():
+    try:
+        req_data = request.get_json()
+        title = req_data['title']
+        content = req_data['content']
+        board_uuid = req_data['board_uuid']
+    except TypeError:
+        return json_response({'errorMsg': 'please send request data'}, 400)
+    except KeyError:
+        return json_response({'errorMsg': 'please check your request data'}, 400)
 
-        board = Board.find_board_by_uuid(board_uuid)
-        if not board:
-            return json_response({'errorMsg': 'board does not exist'})
+    if len(title) == 0 or len(content) == 0:
+        return json_response({'errorMsg': 'please check title and content'}, 400)
 
-        user_id = g.user['id']
+    board = Board.find_board_by_uuid(board_uuid)
+    if not board:
+        return json_response({'errorMsg': 'board does not exist'})
 
-        article = Article(
-            title=title,
-            content=content,
-            board_id=board.id,
-            user_id=user_id
-        )
-        article_uuid = article.save()
-        board.update_latest_article(article_uuid)
+    user_id = g.user['id']
 
-        return json_response({'uuid': article_uuid}, 201)
+    article = Article(
+        title=title,
+        content=content,
+        board_id=board.id,
+        user_id=user_id
+    )
+    article_uuid = article.save()
 
-    elif request.method == 'PATCH':
-        try:
-            req_data = request.get_json()
-            uuid = req_data['uuid']
-            new_title = req_data['new_title']
-            new_content = req_data['new_content']
-        except TypeError:
-            return json_response({'errorMsg': 'please send request data'}, 400)
-        except KeyError:
-            return json_response({'errorMsg': 'please check your request data'}, 400)
+    return json_response({'uuid': article_uuid}, 201)
 
-        article = Article.find_article_by_uuid(uuid)
-        if not article:
-            return json_response({'errorMsg': 'article does not exist'}, 404)
 
-        user_id = g.user['id']
-        if article.user_id != user_id:
-            return json_response({'errorMsg': 'permission denied'}, 403)
+@article_api.route('/article', methods=['PATCH'])
+@Auth.token_required
+def update_article():
+    try:
+        req_data = request.get_json()
+        uuid = req_data['uuid']
+        new_title = req_data['new_title']
+        new_content = req_data['new_content']
+    except TypeError:
+        return json_response({'errorMsg': 'please send request data'}, 400)
+    except KeyError:
+        return json_response({'errorMsg': 'please check your request data'}, 400)
 
-        article_uuid = article.update(new_title, new_content)
-        return json_response({'uuid': article_uuid}, 200)
+    if len(new_title) == 0 or len(new_content) == 0:
+        return json_response({'errorMsg': 'please check title and content'}, 400)
 
-    elif request.method == 'DELETE':
-        try:
-            uuid = request.args.get('uuid')
-        except TypeError:
-            return json_response({'errorMsg': 'please send request data'}, 400)
-        except KeyError:
-            return json_response({'errorMsg': 'please check your request data'}, 400)
+    article = Article.find_article_by_uuid(uuid)
+    if not article:
+        return json_response({'errorMsg': 'article does not exist'}, 404)
 
-        article = Article.find_article_by_uuid(uuid)
-        if not article:
-            return json_response({'errorMsg': 'article does not exsit'}, 404)
+    user_id = g.user['id']
+    if article.user_id != user_id:
+        return json_response({'errorMsg': 'permission denied'}, 403)
 
-        user_id = g.user['id']
-        if article.user_id != user_id:
-            return json_response({'errorMsg': 'permission denied'}, 403)
+    article_uuid = article.update(new_title, new_content)
+    return json_response({'uuid': article_uuid}, 200)
 
-        article.delete()
-        return json_response({}, 204)
+
+@article_api.route('/article/<string:uuid>', methods=['DELETE'])
+@Auth.token_required
+def delete_article(uuid: str):
+    article = Article.find_article_by_uuid(uuid)
+    if not article:
+        return json_response({'errorMsg': 'article does not exist'}, 404)
+
+    user_id = g.user['id']
+    if article.user_id != user_id:
+        return json_response({'errorMsg': 'permission denied'}, 403)
+
+    article.delete()
+    return json_response({}, 204)
