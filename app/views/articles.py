cfa9ea38703bc4dba1a570e84c98f92d2c51dddd
SungminSo <soos196@hanyang.ac.kr>
2020-09-06 19:31:18 +0900

    create article get_articles

--- app/views/articles.py
+++ app/views/articles.py
@@ -8,10 +8,36 @@ from ..shared.auth import Auth
 article_api = Blueprint('article', __name__)
 
 
-@article_api.route("/article", methods=['POST'])
+@article_api.route("/article", methods=['GET', 'POST'])
 @Auth.token_required
 def article_views():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        try:
+            board_uuid = request.args.get('board')
+        except TypeError:
+            return json_response({'errorMsg': 'please send request data'}, 400)
+        except KeyError:
+            return json_response({'errorMsg': 'please check your request data'}, 400)
+
+        board = Board.find_board_by_uuid(board_uuid)
+        if not board:
+            return json_response({'errorMsg': 'board does not exist'}, 404)
+
+        articles = Article.get_articles_by_board(board.id)
+        ret_articles = {}
+
+        for article in articles:
+            ret_articles[article.title] = {
+                "uuid": article.uuid,
+                "title": article.title,
+                "content": article.content,
+                "created_at": article.created_at,
+                "updated_at": article.updated_at
+            }
+
+        return json_response({'articles': ret_articles}, 200)
+
+    elif request.method == 'POST':
         try:
             req_data = request.get_json()
             title = req_data['title']
