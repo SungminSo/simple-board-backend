8a7509e4b9e6e76ca4d890103502df9672a9f10f
SungminSo <soos196@hanyang.ac.kr>
2020-09-06 12:56:29 +0900

    create users log-in

--- app/views/users.py
+++ app/views/users.py
@@ -7,7 +7,7 @@ user_api = Blueprint('user', __name__)
 user_schema = UserSchema
 
 
-@user_api.route("/user", methods=['POST'])
+@user_api.route("/sign-up", methods=['POST'])
 def sign_up():
     if not request.method == 'POST':
         return json_response({'errorMsg': 'invalid method'}, 405)
@@ -29,3 +29,26 @@ def sign_up():
 
     return json_response({'uuid': user_uuid}, 201)
 
+
+@user_api.route("/log-in", methods=['POST'])
+def log_in():
+    if not request.method == 'POST':
+        return json_response({'errorMsg': 'invalid method'}, 405)
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if len(email) == 0 or len(password) == 0:
+        return json_response({'errorMsg': 'please check your email and password'}, 400)
+
+    user = User.find_user_by_email(email)
+
+    if not user:
+        return json_response({'errorMsg': 'invalid email'}, 400)
+
+    if not user.check_password(password):
+        return json_response({'errorMsg': 'invalid password'}, 400)
+
+    token = Auth.generate_user_token(user.uuid, user.email)
+
+    return json_response({'token': token}, 200)
