df246c412612a17f0b4c54e41ae55b118f38e7bb
SungminSo <soos196@hanyang.ac.kr>
2020-09-07 13:26:15 +0900

    fix config.py

--- app/config.py
+++ app/config.py
@@ -10,52 +10,32 @@ def get_env_variable(name: str) -> str:
 
 
 class Config:
-    DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
     POSTGRES_HOST = get_env_variable("POSTGRES_HOST")
     POSTGRES_PORT = get_env_variable("POSTGRES_PORT")
     POSTGRES_USER = get_env_variable("POSTGRES_USER")
     POSTGRES_DB_NAME = get_env_variable("POSTGRES_DB_NAME")
     POSTGRES_PASSWORD = get_env_variable("POSTGRES_PASSWORD")
     SQLALCHEMY_DATABASE_URI = f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     USER_JWT_SECRET_KEY = get_env_variable("USER_JWT_SECRET_KEY")
+
     CORS_ORIGINS = get_env_variable("CORS_ORIGINS")
     CORS_METHODS = get_env_variable("CORS_METHODS")
     CORS_HEADERS = get_env_variable("CORS_HEADERS")
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
 
 
 class TestConfig(Config):
     DEBUG = True
     TESTING = True
 
-    POSTGRES_HOST = get_env_variable("POSTGRES_HOST")
-    POSTGRES_PORT = get_env_variable("POSTGRES_PORT")
-    POSTGRES_USER = get_env_variable("POSTGRES_USER")
-    POSTGRES_DB_NAME = get_env_variable("POSTGRES_DB_NAME")
-    POSTGRES_PASSWORD = get_env_variable("POSTGRES_PASSWORD")
-    SQLALCHEMY_DATABASE_URI = f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB_NAME}"
-    USER_JWT_SECRET_KEY = get_env_variable("USER_JWT_SECRET_KEY")
-    CORS_ORIGINS = get_env_variable("CORS_ORIGINS")
-    CORS_METHODS = get_env_variable("CORS_METHODS")
-    CORS_HEADERS = get_env_variable("CORS_HEADERS")
-
 
 class ProductionConfig(Config):
-    POSTGRES_HOST = get_env_variable("POSTGRES_HOST")
-    POSTGRES_PORT = get_env_variable("POSTGRES_PORT")
-    POSTGRES_USER = get_env_variable("POSTGRES_USER")
-    POSTGRES_DB_NAME = get_env_variable("POSTGRES_DB_NAME")
-    POSTGRES_PASSWORD = get_env_variable("POSTGRES_PASSWORD")
-    SQLALCHEMY_DATABASE_URI = f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB_NAME}"
-    USER_JWT_SECRET_KEY = get_env_variable("USER_JWT_SECRET_KEY")
-    CORS_ORIGINS = get_env_variable("CORS_ORIGINS")
-    CORS_METHODS = get_env_variable("CORS_METHODS")
-    CORS_HEADERS = get_env_variable("CORS_HEADERS")
+    DEBUG = False
 
 
 config_by_name = dict(
